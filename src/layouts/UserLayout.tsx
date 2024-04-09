5641563a27db2922977991f2846dea1ea25c9865
723poil <leetkdguq73@naver.com>
2024-04-10 00:57:21 +0900

    2024-04-10 필요한 페이지 정리

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,9 +1,8 @@
 // ** React Imports
-import { ReactNode } from 'react'
+import {ReactNode} from 'react'
 
 // ** MUI Imports
-import Box from '@mui/material/Box'
-import { Theme } from '@mui/material/styles'
+import {Theme} from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 
 // ** Layout Imports
@@ -14,66 +13,50 @@ import VerticalLayout from '../@core/layouts/VerticalLayout'
 import VerticalNavItems from '../navigation/vertical'
 
 // ** Component Import
-import UpgradeToProButton from './components/UpgradeToProButton'
 import VerticalAppBarContent from './components/vertical/AppBarContent'
 
 // ** Hook Import
-import { useSettings } from '../@core/hooks/useSettings'
+import {useSettings} from '../@core/hooks/useSettings'
 
 interface Props {
-  children: ReactNode
+    children: ReactNode
 }
 
-const UserLayout = ({ children }: Props) => {
-  // ** Hooks
-  const { settings, saveSettings } = useSettings()
+const UserLayout = ({children}: Props) => {
+    // ** Hooks
+    const {settings, saveSettings} = useSettings()
 
-  /**
-   *  The below variable will hide the current layout menu at given screen size.
-   *  The menu will be accessible from the Hamburger icon only (Vertical Overlay Menu).
-   *  You can change the screen size from which you want to hide the current layout menu.
-   *  Please refer useMediaQuery() hook: https://mui.com/components/use-media-query/,
-   *  to know more about what values can be passed to this hook.
-   *  ! Do not change this value unless you know what you are doing. It can break the template.
-   */
-  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'))
+    /**
+     *  The below variable will hide the current layout menu at given screen size.
+     *  The menu will be accessible from the Hamburger icon only (Vertical Overlay Menu).
+     *  You can change the screen size from which you want to hide the current layout menu.
+     *  Please refer useMediaQuery() hook: https://mui.com/components/use-media-query/,
+     *  to know more about what values can be passed to this hook.
+     *  ! Do not change this value unless you know what you are doing. It can break the template.
+     */
+    const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'))
 
-  const UpgradeToProImg = () => {
     return (
-      <Box sx={{ mx: 'auto' }}>
-        <a
-          target='_blank'
-          rel='noreferrer'
-          href='https://themeselection.com/products/materio-mui-react-nextjs-admin-template/'
+        <VerticalLayout
+            hidden={hidden}
+            settings={settings}
+            saveSettings={saveSettings}
+            verticalNavItems={VerticalNavItems()} // Navigation Items
+            // afterVerticalNavMenuContent={UpgradeToProImg}
+            verticalAppBarContent={(
+                props // AppBar Content
+            ) => (
+                <VerticalAppBarContent
+                    hidden={hidden}
+                    settings={settings}
+                    saveSettings={saveSettings}
+                    toggleNavVisibility={props.toggleNavVisibility}
+                />
+            )}
         >
-          <img width={230} alt='upgrade to premium' src={`/images/misc/upgrade-banner-${settings.mode}.png`} />
-        </a>
-      </Box>
+            {children}
+        </VerticalLayout>
     )
-  }
-
-  return (
-    <VerticalLayout
-      hidden={hidden}
-      settings={settings}
-      saveSettings={saveSettings}
-      verticalNavItems={VerticalNavItems()} // Navigation Items
-      afterVerticalNavMenuContent={UpgradeToProImg}
-      verticalAppBarContent={(
-        props // AppBar Content
-      ) => (
-        <VerticalAppBarContent
-          hidden={hidden}
-          settings={settings}
-          saveSettings={saveSettings}
-          toggleNavVisibility={props.toggleNavVisibility}
-        />
-      )}
-    >
-      {children}
-      <UpgradeToProButton />
-    </VerticalLayout>
-  )
 }
 
 export default UserLayout
